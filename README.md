734833346c70e09b3f931ffd12a703f4039da498
Kedar Kulkarni <kedar.kulkarni12@gmail.com>
2021-09-28 18:09:17 +0530

    fix: answers are now not case sensitive

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # CLI Quiz About Myself
 
-A CLI Quiz about myself created on replit.com using JS
+A CLI Quiz about myself created on replit.com using Javascript
--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const chalk = require('chalk');
 
 var readlineSync = require('readline-sync');
 var score = 0;
-
+var userName = readlineSync.question("What is you Good Name?\n");
+console.log("Welcome " +chalk.green(userName)+" to the"+chalk.blue( " My Quiz!"));
 
 function play(question,answer){
 var getAnswer = readlineSync.question(question);
-if (answer===getAnswer){
+if (answer.toUpperCase()===getAnswer.toUpperCase()  ){
  console.log(chalk.green("Yay! You are Correct"));
  score = score + 1;
  }
@@ -19,8 +20,8 @@ else{
 
  
 var qna = [
-  { question:"Q. What is my Name?\n ",
-    answer:"Kedar"},
+  { question:"Q. What is my Surname?\n ",
+    answer:"Kulkarni"},
   { question: "Q. Is my age greater than 25?\n",
     answer: "No" },
   { question: "Q. What is my favourite Sport?\n",
